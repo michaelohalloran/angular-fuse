656416cce9b56600826dde77309520e70425152f
michael.ohalloran@polarisalpha.com <michael.ohalloran@polarisalpha.com>
2019-02-07 08:07:39 -0700

    add angular animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,8 @@ import { AnimateComponent } from './composite/animate/animate.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/composite/animate/animate.component.ts
+++ src/app/composite/animate/animate.component.ts
@@ -1,32 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, animate, state, style, transition } from '@angular/animations';
 
 @Component({
   selector: 'animate',
   templateUrl: './animate.component.html',
-  styleUrls: ['./animate.component.css']
+  styleUrls: ['./animate.component.css'],
+  animations: [
+    // trigger('colorChange', [
+    //   transition('* => *', [
+    //     style({opacity: 0.2, backgroundColor: 'green', transform: 'scale(1)'}),
+    //     animate('4000ms ease-in', 
+    //     style({opacity: 1, backgroundColor: 'red', transform: 'scale(1.5)'}))
+    //   ])
+    // ])
+    trigger('colorChange', [
+      state('start', style({opacity: 0.8, backgroundColor: 'green', transform: 'scale(1.5)'})),
+      state('end', style({opacity: 1, backgroundColor: 'lightsalmon', transform: 'scale(1)'})),
+      transition('* => *', animate(1000))
+    ]),
+    trigger('textChange', [
+      state('init', style({opacity: 0.2, backgroundColor: 'mediumturquoise', color: 'blue', transform: 'scale(1)'})),
+      state('final', style({opacity: 1, backgroundColor: 'blueviolet', padding: '10%', color: 'white', transform: 'scale(1.2)'})),
+      transition('init<=>final', animate(2000)),
+      // transition('final=>init', animate(2000)),
+    ])
+  ]
 })
 export class AnimateComponent implements OnInit {
 
   userInput: string = '';
   defaultStartText: string = '';
   defaultEndText: string = '';
+  private changeColor: string = '';
+  private changeText: string = '';
 
   constructor() { }
 
   ngOnInit() {
     this.defaultStartText = 'Click to start animation';
-    this.defaultEndText = 'Click to revert animation';
+    this.defaultEndText = 'Click to reset animation state';
   }
 
   startAnim() {
     console.log('hit startAnim');
     //replace text in start box with input
-    this.defaultStartText = this.userInput;
+    
     //animate background color and text?
+    this.changeColor = 'start';
+    this.defaultStartText = this.userInput;
+    this.changeText = this.changeText === 'init' ? 'final': 'init';
   }
 
   revertAnim() {
     console.log('hit revertAnim');
+    this.changeColor = 'end';
+    this.changeText = '';
   }
 
 }
